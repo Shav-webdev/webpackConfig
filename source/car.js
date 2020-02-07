9a2104229ff686a804b14f98896e071407018fcd
Shavarsh <shavarsh.abrahamyan1993@gmail.com>
2020-02-07 15:16:39 +0400

    added file-loader, css-loader and style-loader, to project, added watch script watcher

--- source/car.js
+++ source/car.js
@@ -1,9 +1,12 @@
 import Client from "./client";
+import styles from "../source/style.css"
+import pic from "./assets/pic.jpeg"
 
 class Car {
-    constructor(model, color, date) {
+    constructor(model, color, img) {
         this.model = model;
         this.color = color;
+        this.img = img
     }
 
     getModel() {
@@ -12,14 +15,20 @@ class Car {
     getColor() {
         return this.color;
     }
+    getImage() {
+        return JSON.stringify({img : this.img});
+    }
 }
 
 const bmw = new Car("BMW", "black").getModel();
 const volvo = new Car("Volvo", "white").getModel();
+const webpack = new Car("Webpack", "white").getImage();
 
 const client = new Client("John", bmw);
 const client1 = new Client("Jack", volvo);
+const client2 = new Client("Webpack", webpack);
 
 console.log(bmw);
 console.log(client);
 console.log(client1);
+console.log(client2);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,33 @@
-const path = require('path')
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} =require('clean-webpack-plugin')
 
 module.exports = {
+    context: path.resolve(__dirname, "source"),
     mode: "development",
     entry: {
-        main: './src/car.js',
+        main: './car.js',
     },
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "build")
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./index.html"
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/,
+                use: ["file-loader"]
+            }
+        ]
     }
-}
+};
